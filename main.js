f0779fdc103ab4e41b29b078d06e505c9dce050f
Irina Bojkovska <irina.bojkovska.ib@gmail.com>
2024-05-18 21:39:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,3 @@
-let age = 19;
-let checkAge = age > 18 ? "You can vote" : "You cannot vote";
-console.log(checkAge);
-
-let test = 100;
-
-if (test <= 65) {
-  console.log("You failed!");
-} else if (test <= 100) {
-  console.log("You passed!");
-}
-
-let checkTest = test <= 65 ? "You passed!" : "You failed!";
-console.log(checkTest);
-
-let temperature = 20;
-
-if (temperature <= 25) {
-  console.log("It's warm outside");
-} else if (temperature <= 20) {
-  console.log("It's cold outside");
-}
-
-let checkTemperature =
-  temperature < 10 ? "It's warm outside" : "It's cold outside";
-console.log(checkTemperature);
-
 let price = 130;
 
 if (price <= 100) {
