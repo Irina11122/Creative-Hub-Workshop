b9001c8a38d7361dc16bcf287ec9c78973d2e86f
Irina Bojkovska <irina.bojkovska.ib@gmail.com>
2024-05-18 16:47:17 +0200

    vtora vezba

--- main.js
+++ main.js
@@ -9,3 +9,17 @@ if (age <= 12) {
 } else {
   console.log("Senior ticket: $12");
 }
+
+// Vezba 2
+
+let day = "monday";
+
+if (day == "monday") {
+  console.log("Start of the work week!");
+} else if (day == "wednesday") {
+  console.log("Midweek!");
+} else if (day == "friday") {
+  console.log("Almost weekend!");
+} else {
+  console.log("Just another day.");
+}
