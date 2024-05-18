7b097126f0b0a55aff1bebf4209af811d7cff436
Irina Bojkovska <irina.bojkovska.ib@gmail.com>
2024-05-18 16:48:22 +0200

    fixed

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-let age = 29;
-
-if (age <= 12) {
-  console.log("Child ticket: $5");
-} else if (age <= 18) {
-  console.log("Teen ticket: $7");
-} else if (age <= 60) {
-  console.log("Adult ticket: $10");
-} else {
-  console.log("Senior ticket: $12");
-}
 
 // Vezba 2
 
