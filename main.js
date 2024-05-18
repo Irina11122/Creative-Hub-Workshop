a845d0af366236840b5c1b9d5928af5be24b2394
Irina Bojkovska <irina.bojkovska.ib@gmail.com>
2024-05-18 23:20:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-let age = 19;
-let checkAge = age > 18 ? "You can vote" : "You cannot vote";
-console.log(checkAge);
+let age = 18;
+if (age <= 17) {
+  console.log("You can't vote!");
+} else if (age >= 18) {
+  console.log("You can vote!");
+}
 
+let checkAge = age > 17 ? "You can vote" : "You cannot vote";
+console.log(checkAge);
