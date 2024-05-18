a8d828d34ef989b0df764caca02f001448d8b063
Irina Bojkovska <irina.bojkovska.ib@gmail.com>
2024-05-18 18:33:21 +0200

    Update main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+let username = "admin";
+let password = "password123";
+
+if ((username === "admin", password === "adminpass")) {
+  console.log("Welcome Admin");
+} else if ((username == "user123" % password === "password123")) {
+  console.log("Welcome User");
+} else {
+  console.log(" Invalid username or password");
+}
